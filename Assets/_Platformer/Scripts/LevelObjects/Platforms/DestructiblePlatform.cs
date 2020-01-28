0df88284cb09307845fe3ba7262d6db1878901bd
maximiliensk <maximilien.sadi.k@gmail.com>
2020-01-28 10:39:25 +0100

    destructible and moving platforms

--- Assets/_Platformer/Scripts/LevelObjects/Platforms/DestructiblePlatform.cs
+++ Assets/_Platformer/Scripts/LevelObjects/Platforms/DestructiblePlatform.cs
@@ -6,14 +6,27 @@
 using UnityEngine;
 
 namespace Com.IsartDigital.Platformer.LevelObjects.Platforms {
-	public class DestructiblePlatform : MonoBehaviour {
-	
-		private void Start () {
-			
-		}
-		
-		private void Update () {
-			
-		}
-	}
+	public class DestructiblePlatform : Platform {
+
+        private bool startCount = false;
+        [SerializeField] private float duration;
+        private float elapsedTime;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            startCount = true;
+        }
+
+        private void Update()
+        {
+            if (startCount)
+            {
+                elapsedTime += Time.deltaTime;
+
+                if (elapsedTime > duration) transform.Find("collider").GetComponent<PolygonCollider2D>().enabled = false;
+            }
+        }
+
+
+    }
 }

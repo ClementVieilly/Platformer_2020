27ede44831885b8797d4fbb407bc6f2ccf3e9c47
maximiliensk <maximilien.sadi.k@gmail.com>
2020-02-19 16:36:30 +0100

    Sound integration (fall sound missing + waiting for player.cs to be free to play some missing sounds)

--- Assets/_Platformer/Scripts/ScriptableObjects/SoundsSettings.cs
+++ Assets/_Platformer/Scripts/ScriptableObjects/SoundsSettings.cs
@@ -20,13 +20,19 @@ namespace Com.IsartDigital.Platformer {
         [SerializeField] private string _character_Plane_Flap01; 
         [SerializeField] private string _character_Plane_Flap02; 
         [SerializeField] private string _character_Plane_Wind; 
+        [SerializeField] private string _ambiance_level_one; 
+        [SerializeField] private string _ambiance_level_two; 
 
 
         public string FootstepsStone { get => _character_Footsteps_Stone;  }
         public string FootstepsWood { get => _character_Footsteps_Wood;  }
         public string Jump { get => _character_Jump;  }
         public string WallJump { get => _character_WallJump;  }
+        public string Landing { get => _character_Landing;  }
         public string PlaneFlap01 { get => _character_Plane_Flap01;  }
+        public string PlaneFlap02 { get => _character_Plane_Flap02;  }
         public string PlaneWind { get => _character_Plane_Wind;  }
+        public string Ambiance_Level_One { get => _ambiance_level_one;  }
+        public string Ambiance_Level_Two { get => _ambiance_level_two;  }
     }
 }

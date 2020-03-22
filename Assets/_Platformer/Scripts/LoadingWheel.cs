0305adc89ec2b75859bfed1000db6aecaa888402
Joël Voignier <jo.voignier@student.isartdigital.com>
2020-03-23 00:11:57 +0100

    Wheel that gonna be used for server loading is started

---
+++ Assets/_Platformer/Scripts/LoadingWheel.cs
@@ -0,0 +1,46 @@
+///-----------------------------------------------------------------
+/// Author : Joël VOIGNIER
+/// Date : 22/03/2020 23:28
+///-----------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Com.IsartDigital.Platformer
+{
+	[RequireComponent(typeof(Image))]
+	public class LoadingWheel : MonoBehaviour
+	{
+		private Image wheel = null;
+
+		[SerializeField] private float duration = 1f;
+		private float elapsedTime = 0f;
+
+		[SerializeField] private AnimationCurve animationCurve = null;
+
+		private void Start()
+		{
+			wheel = GetComponent<Image>();
+			wheel.type = Image.Type.Filled;
+			wheel.fillMethod = Image.FillMethod.Radial360;
+			wheel.fillClockwise = true;
+			wheel.fillAmount = 1f;
+		}
+
+		private void Update()
+		{
+			elapsedTime += Time.deltaTime;
+
+			if (wheel.fillClockwise)
+				wheel.fillAmount = Mathf.Lerp(1f, 0f, animationCurve.Evaluate(Mathf.Clamp01(elapsedTime / duration)));
+			else
+				wheel.fillAmount = Mathf.Lerp(0f, 1f, animationCurve.Evaluate(Mathf.Clamp01(elapsedTime / duration)));
+
+			if (elapsedTime >= duration)
+			{
+				elapsedTime = 0f;
+				wheel.fillClockwise = !wheel.fillClockwise;
+			}
+		}
+	}
+}

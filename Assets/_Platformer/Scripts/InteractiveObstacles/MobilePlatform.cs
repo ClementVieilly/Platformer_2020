2b28d1fe2d080bd9c654f188ff18dccd63f49f2c
Joël Voignier <jo.voignier@student.isartdigital.com>
2020-03-22 22:58:45 +0100

    Fix spider prefab + mobile platforms

--- Assets/_Platformer/Scripts/InteractiveObstacles/MobilePlatform.cs
+++ Assets/_Platformer/Scripts/InteractiveObstacles/MobilePlatform.cs
@@ -97,9 +97,9 @@ namespace Com.IsartDigital.Platformer.InteractiveObstacles {
             }
         }
 
-        private void OnCollisionEnter2D(Collision2D collision)
+        private void OnCollisionStay2D(Collision2D collision)
         {
-            if (collision.collider.CompareTag(playerTag))
+            if (!touchedObject && collision.collider.CompareTag(playerTag))
                 touchedObject = collision.transform;
         }
 

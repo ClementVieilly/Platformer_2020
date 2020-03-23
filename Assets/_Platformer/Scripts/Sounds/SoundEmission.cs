a638fed62d8e1b38ca5beeb13afc09e787465926
alexandre_raumel <alexandre.raumel@gmail.com>
2020-03-23 13:17:24 +0100

    Adds script soundemission for particle system

--- Assets/_Platformer/Scripts/Sounds/SoundEmission.cs
+++ Assets/_Platformer/Scripts/Sounds/SoundEmission.cs
@@ -3,19 +3,44 @@
 /// Date : 23/03/2020 12:50
 ///-----------------------------------------------------------------
 
+using Com.IsartDigital.Platformer.LevelObjects;
 using UnityEngine;
 
 namespace Com.IsartDigital.Platformer.Sounds {
-	public class SoundEmission : MonoBehaviour
+	public class SoundEmission : ALevelObject
 	{
+		[SerializeField] ParticleType emitParticle;
+
+		private string soundToPlay = null;
+
 	    void Start()
 	    {
-	        
-	    }
+			switch (emitParticle)
+			{
+				case ParticleType.NONE:
+					soundToPlay = null;
+					break;
+				case ParticleType.Sparks:
+					soundToPlay = sounds.Env_electric_sparks;
+					break;
+				case ParticleType.ElectricityArc:
+					soundToPlay = sounds.Env_electric_arc;
+					break;
+				default:
+					break;
+			}
+		}
 	
 	    void Update()
 	    {
 	        
 	    }
+	}
+
+	public enum ParticleType
+	{
+		NONE,
+		Sparks,
+		ElectricityArc,
 	}
 }
